e0c07eee389ed4ae70235f5a924abfd36df6faea
PEACE-DFG <awofesobipeace@gmail.com>
2023-07-03 22:12:23 +0100

    validation of register remaining repeat password

--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+// import { useState } from 'react'
 function Register() {
+  const [user,setUser]=useState({
+    Matricnumber:'',
+    Level:'',
+    RepeatPassword: '',
+    Password:''
+  });
+  const [errors,setErrors]=useState({});
+  const submitData=(e)=>{
+    e.preventDefault();
+    const newErrors={};
+    
+    if(user.Matricnumber.trim()== ''){
+      newErrors.MatricnumberErr='Your Matric Number is Required'
+    }
+    if (user.Password.trim() === '') {
+      newErrors.PasswordErr = 'Your Password is required';
+    }
+    
+    if (user.RepeatPassword.trim() === '') {
+      newErrors.RepeatPasswordErr = 'Your Password is required';
+    }
+    if (user.Level.trim() === '') {
+      newErrors.LevelErr = 'Your Level is required';
+    }
+    if (user.Password.trim().length != user.RepeatPassword.trim().length){
+      newErrors.RepeatPasswordErr='Passwords do not match';
+    
+    }
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    }
+    else {
+      let users = [];
+      if (localStorage.getItem('users')) {
+        users = JSON.parse(localStorage.getItem('users'));
+      }
+
+      users.push(user);
+      const dataUsers = JSON.stringify(users);
+      localStorage.setItem('users', dataUsers);
+    }
+  }
   return (
-    <div style={{backgroundImage:'url(https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg)',backgroundRepeat:'no-repeat',backgroundSize:'cover',height:'100vh'}}>
+    <div style={{backgroundImage:'url(https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg)',backgroundRepeat:'no-repeat',backgroundSize:'cover',height:'120vh'}}>
                <div style={{backgroundColor:'rgb(91, 150, 2)'}} >
         <div className='container top text-light'  >
            <header className='d-flex '>
@@ -99,7 +141,7 @@ function Register() {
   </div>
 </nav>
 <section className='rew container mt-5'>
-<form action="" className='rat'>
+<form action="" className='rat'  onSubmit={submitData}>
 <div className='rode mt-5' style={{maxWidth:'500px',margin:'auto'}}>
     <div className='root text-center'>
         <img src="https://uniosun.edu.ng/templates/uniosun/images/styling/green/logo.png" className='w-75 img-fluid' alt="" />
@@ -108,26 +150,56 @@ function Register() {
     </h1>
     </div>
 <div class="form-floating my-3">
-  <input type="text" class="form-control" id="floatingInput" placeholder="matricnumber"/>
-  <label for="floatingInput">Matric Number</label>
+  <input type="text" class="form-control" id="floatingInput" placeholder="Matricnumber"
+   onChange={(e) => {
+    setUser({ ...user, Matricnumber: e.target.value });
+  }}/>
+  <label for="floatingInput" htmlFor="Matricnumber">Matric Number</label>
+  {errors.MatricnumberErr && <span style={{ color: 'red' }}>{errors.MatricnumberErr}</span>}
 </div>
 {/* <div class="form-floating mb-3">
   <input type="text" class="form-control" id="floatingInput" placeholder="leve"/>
   <label for="floatingInput">Email address</label>
 </div> */}
-<select class="form-select my-3" aria-label="Default select example">
+<select class="form-select my-3" aria-label="Default select example" htmlFor="Level"
+ onChange={(e) => {
+  setUser({ ...user, Level: e.target.value });
+}}>
   <option selected>Select Your Level</option>
-  <option value="1">200 level</option>
-  <option value="2">300 level</option>
-  <option value="3">400 level</option>
-  <option value="3">500 level</option>
+  <option value="200 Level">200 level</option>
+  <option value="300 Level">300 level</option>
+  <option value="400 Level">400 level</option>
+  <option value="500 Level">500 level</option>
 </select>
+{errors.LevelErr && <span style={{ color: 'red' }}>{errors.LevelErr}</span>}
+
 <div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-  <label for="floatingPassword">Password</label>
-</div>
+                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                  onChange={(e)=>{
+                     setUser({...user, Password:e.target.value})
+                   }}/>
+                    <span style={{color:'red'}}>
+                             {errors.PasswordErr}
+
+                             </span>
+                 <label for="floatingPassword" name='Password'>Password</label>
+                 </div>
+                 <div class="form-floating mt-3">
+                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                  onChange={(e)=>{
+                     setUser({...user, RepeatPassword:e.target.value})
+                   }}/>
+                    <span style={{color:'red'}}>
+                             {errors.RepeatPasswordErr}
+
+                             </span>
+                             {/* <span>
+                              {newRepeat}
+                             </span> */}
+                 <label for="floatingPassword" name='RepeatPassword'>RepeatPassword</label>
+                 </div> 
 <div class="col-12 text-center">
-    <button class="btn btn-success my-3" type="submit">Submit form</button>
+    <button class="btn btn-success my-3" type="submit">Register</button>
   </div>
 </div>
 </form>
